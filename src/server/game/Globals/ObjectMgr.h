jairoe05/tc_duplicate

Core/Objects: Fix typo in 19cab2c50857045788bd80e202ed661e49ed8bba

src/server/game/Globals/ObjectMgr.h
@@ -57,7 +57,7 @@ struct PlayerLevelInfo;
 struct PageText
 {
     std::string Text;
-    uint16 NextPage;
+    uint32 NextPage;
 };
 
 /// Key for storing temp summon data in TempSummonDataContainer

The-Legion-Preservation-Project/AshamaneCore

(cherry picked from commit dbe854cd313ed23e86c67665228a876b7c23c7fc)
Conflicts:
	src/server/game/Globals/ObjectMgr.h

src/server/game/Globals/ObjectMgr.h
@@ -55,7 +55,7 @@ struct PlayerLevelInfo;
 struct PageText
 {
     std::string Text;
-    uint16 NextPageID;
+    uint32 NextPageID;
 };
 
 /// Key for storing temp summon data in TempSummonDataContainer