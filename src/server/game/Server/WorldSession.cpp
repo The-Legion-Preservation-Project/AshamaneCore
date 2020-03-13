allankrdec/TrinityCore-10-2024

Core/Server: fix duplicated inserts into tutorials table for newly created characters

src/server/game/Server/WorldSession.cpp
@@ -780,6 +780,10 @@ void WorldSession::SaveTutorialsData(SQLTransaction &trans)
     stmt->setUInt32(MAX_ACCOUNT_TUTORIAL_VALUES, GetAccountId());
     trans->Append(stmt);
 
+    // now has, set flag so next save uses update query
+    if (!hasTutorialsInDB)
+        m_TutorialsChanged |= TUTORIALS_FLAG_LOADED_FROM_DB;
+
     m_TutorialsChanged &= ~TUTORIALS_FLAG_CHANGED;
 }
 

The-Legion-Preservation-Project/AshamaneCore

(cherrypicked from 60dd803bbe01283b15e2b2c5b02d8f857b96745a)

src/server/game/Server/WorldSession.cpp
@@ -834,7 +834,11 @@ void WorldSession::SaveTutorialsData(SQLTransaction& trans)
     stmt->setUInt32(MAX_ACCOUNT_TUTORIAL_VALUES, GetAccountId());
     trans->Append(stmt);
 
-    _tutorialsChanged = false;
+    // now has, set flag so next save uses update query
+    if (!hasTutorialsInDB)
+        _tutorialsChanged |= TUTORIALS_FLAG_LOADED_FROM_DB;
+
+    _tutorialsChanged &= ~TUTORIALS_FLAG_CHANGED;
 }
 
 bool WorldSession::IsAddonRegistered(const std::string& prefix) const